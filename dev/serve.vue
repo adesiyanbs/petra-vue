4bf90fdae4b10ac7e88663b1ffca27552e4c825b
Emmanuel Adebayo <Emmanueltolu.adebayo@gmail.com>
2021-09-14 14:00:18 +0100

    feat: vue plugin

---
+++ dev/serve.vue
@@ -0,0 +1,59 @@
+<script>
+import Vue from 'vue';
+import PetraVue from '@/petra-vue.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    PetraVue,
+  },
+  data() {
+    return {
+      data:{
+        amount: 100000,
+        email: 'emmanueltolu.adebayo@gmail.com',
+        key:'pk_test_U4GzNZdtkqC0Oxkdi4FkmH9JLNcE1DKe',
+        id:"8a24eb20-5721-4183-987d-aa6bc371ab60",
+        payType: "p",
+        reference: "",
+        button : {
+          backgroundColor: "#E1b234",
+          border: "none",
+          borderRadius: "10px",
+          color: "white",
+          textAlign: "center",
+          textDecoration: "none",
+          height: "50px",
+          width: "250px",
+          display: "inline-flex",
+          alignItems: 'center',
+          justifyContent: 'center',
+          fontSize: "14px",
+          transitionDuration: "0.4s",
+          cursor: "pointer",
+          padding: "10px 32px",
+          opacity: "1"
+        }
+      }
+    }
+  },
+
+  methods: {
+    onClose(){
+      alert('Payment modal closed')
+    },
+    onSuccess: ()=>{
+      // alert('')
+    }
+  },
+});
+</script>
+
+<template>
+  <div id="app">
+    <petra-vue       :data="data"
+                     :onClose="onClose"
+                     :onSuccess="onSuccess" />
+
+  </div>
+</template>
---
+++ src/petra-vue.vue
@@ -0,0 +1,194 @@
+<script>
+
+export default /*#__PURE__*/{
+  name: 'PetraVue', // vue component name
+  props: ['data', 'onSuccess', 'onClose'],
+  data() {
+    return {
+      myModal:{display: "none"},
+      dbutton2: {
+        backgroundColor: "#F7A200",
+        border: "none",
+        borderRadius: "10px",
+        color: "white",
+        textAlign: "center",
+        textDecoration: "none",
+        height: "50px",
+        width: "250px",
+        display: "inline-flex",
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontSize: "14px",
+        transitionDuration: "0.4s",
+        cursor: "pointer",
+        padding: "10px 32px",
+        opacity: "1"
+      },
+      dModalContent :{
+        backgroundColor: "#fefefe",
+        margin: "auto",
+        padding: "20px",
+        width: "80%",
+        height: "80%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      },
+      dIframe :{
+        display: "none",
+        border: "none",
+        height: "100%",
+        width: "100%",
+        zIndex: "99999",
+      },
+      dModalClose :{
+        color: "#aaaaaa",
+        position: "absolute",
+        top: "5%",
+        right: "8%",
+        float: "right",
+        fontSize: "28px",
+        fontWeight: "bold",
+        cursor: "pointer",
+      },
+      dModal:{
+        display: "none",
+        position: "fixed",
+        zIndex: "1",
+        paddingTop: "20px",
+        left: "0",
+        top: "0",
+        width: "100%",
+        height: "100%",
+        overflow: "auto",
+        // backgroundColor: "rgb(0,0,0)",
+        backgroundColor: "rgba(0,0,0,0.4)",
+      },
+      loaderWrapper: {
+        height: "100vh",
+        display: "grid",
+        placeItems: "center",
+      },
+      loaderImg: {
+        height: "100px",
+        width: "100px",
+        display: "block",
+        animation: "loader 1s 1s infinite",
+      }
+    }
+  },
+  methods: {
+    mover(){
+      this.dbutton2.opacity = "0.8"
+    },
+
+    moout() {
+      this.dbutton2.opacity = "1"
+    },
+
+    openIframe() {
+      this.handleAnimation();
+      this.dModal.display = 'block'
+     // this.$refs.dFrame.addEventListener('load', this.handleIframeLoaded, true)
+      this.iframeData(this)
+    },
+
+    handleIframeLoaded() {
+   //   this.$refs.dFrame.removeEventListener('load', this.handleIframeLoaded, true)
+      this.$refs.loaderImg.style.display = "none";
+      this.$refs.dFrame.style.display = "block";
+      console.log('HANDLE LOAD')
+    },
+
+    closeModal(data){
+      if(data) data()
+      // eslint-disable-next-line no-self-assign
+      this.$refs.dFrame.src = this.$refs.dFrame.src
+      this.dModal.display = 'none'
+    },
+
+    handleAnimation() {
+      this.$refs.dFrame.style.display = 'none'
+      const loader = this.$refs.loaderImg;
+      if (!loader) {
+        loader.setAttribute('src', 'https://www.thepetra.co/favicon.ico')
+      }
+      loader.style.display = 'block'
+      loader.animate([
+        // keyframes
+        { transform: 'scale(0.7)' },
+        { transform: 'scale(0.8)' },
+        { transform: 'scale(0.7)' }
+      ], {
+        // timing options
+        duration: 1000,
+        iterations: Infinity
+      });
+      setTimeout(() => {
+        this.$refs.loaderImg.style.display = "none";
+        this.$refs.dFrame.style.display = "block";
+      }, 7000);
+    },
+
+    iframeData(dataFunc){
+      window.addEventListener('message', function(event) {
+        if (event.origin == 'https://www.thepetra.co/'){
+          if(event.data == 'false pass'){
+            dataFunc.closeModal(dataFunc.onClose)
+
+          }
+          if(event.data == 'false pass1'){
+            dataFunc.closeModal(dataFunc.onSuccess)
+          }
+        }
+      })
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <button
+        v-html="`Checkout with Petra <img width='35' src='https://www.thepetra.co/favicon.ico' />`"
+        :style="data.button"
+        @mouseover="mover()"
+        @mouseleave="moout()"
+        @click="openIframe()"
+    >
+
+    </button>
+    <br><br>
+
+    <div :style="dModal">
+      <div :style="dModalContent">
+        <div ref="loaderWrapper" id="loaderWrapper" :style="loaderWrapper">
+          <img ref="loaderImg" alt="loader" :style="loaderImg" id="loaderImg" src="https://www.thepetra.co/favicon.ico" />
+        </div>
+        <span :style="dModalClose" @click="closeModal()">X</span>
+        <iframe
+            ref="dFrame"
+            :src="`https://checkout.petra.africa/?amount=${data.amount}&email=${data.email}&key=${data.key}&payType=${data.payType ? data.payType + '&id='+data.id : ''}`"
+            frameborder="0"
+            :style="dIframe"
+        ></iframe>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+  .petra-vue {
+    display: block;
+    width: 400px;
+    margin: 25px auto;
+    border: 1px solid #ccc;
+    background: #eaeaea;
+    text-align: center;
+    padding: 25px;
+  }
+  .petra-vue p {
+    margin: 0 0 1em;
+  }
+</style>
