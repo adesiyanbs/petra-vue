4c3ddd2262261023c3ed649cbe6c40ce4f68688d
Adeoluwa <minneade@gmail.com>
2021-09-16 21:20:36 +0100

    Fix: adjusting

--- src/petra-vue.vue
+++ src/petra-vue.vue
@@ -64,7 +64,7 @@ export default /*#__PURE__*/{
         width: "100%",
         height: "100%",
         overflow: "auto",
-        
+        //changes
         // backgroundColor: "rgb(0,0,0)",
         backgroundColor: "rgba(0,0,0,0.4)",
       },
